39ca4a0b6f07235960ad1837464092187cf4709a
Michael Rittmeister <michael@rittmeister.in>
2023-04-07 13:56:29 +0200

    Fix js target

--- src/main/kotlin/model/targets/JsTargetItem.kt
+++ src/main/kotlin/model/targets/JsTargetItem.kt
@@ -1,25 +1,13 @@
 package dev.kord.gradle.model.targets
 
 import dev.kord.gradle.model.ItemParent
-import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompilerOptions
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.kpm.external.ExternalVariantApi
-import org.jetbrains.kotlin.gradle.kpm.external.project
+import org.jetbrains.kotlin.gradle.plugin.KotlinJsCompilerType
 import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsTargetDsl
-import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrTarget
-import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrTargetPreset
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
-public class JsTargetItem(override val name: String) :
-    AbstractTargetItem<KotlinJsTargetDsl, KotlinJsCompilerOptions>() {
-    @OptIn(ExternalVariantApi::class)
-    override fun KotlinMultiplatformExtension.configure(configure: KotlinJsTargetDsl.() -> Unit) {
-        val target = LenientJsPreset(project).createTarget(name)
-        targets.add(target)
-    }
-}
+public typealias JsTargetItem = AbstractTargetItem<KotlinJsTargetDsl, KotlinJsCompilerOptions>
 
 public inline fun ItemParent.js(
     name: String = "js",
@@ -28,7 +16,11 @@ public inline fun ItemParent.js(
     contract {
         callsInPlace(configure, InvocationKind.EXACTLY_ONCE)
     }
-    addItem(JsTargetItem(name).apply(configure))
+    addTarget(
+        name,
+        { actualName, actualConfigure -> js(actualName, KotlinJsCompilerType.IR, actualConfigure) },
+        configure
+    )
 }
 
 public inline fun ItemParent.nodejs(
@@ -61,12 +53,3 @@ public inline fun ItemParent.fullJs(
     }
 }
 
-// This is a hack which prevents the Kotlin Plugin from getting mad because it misses a js sub target
-private class LenientJsPreset(project: Project) : KotlinJsIrTargetPreset(project) {
-    public override fun instantiateTarget(name: String): KotlinJsIrTarget {
-        @Suppress("INVISIBLE_SETTER")
-        return project.objects.newInstance(KotlinJsIrTarget::class.java, project, platformType, true).apply {
-            isMpp = true
-        }
-    }
-}
